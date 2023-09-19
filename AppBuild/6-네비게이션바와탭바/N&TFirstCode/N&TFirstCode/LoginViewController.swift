340fbd04291ecacce7813720f3b22faf561be251
Sunny <wntjs0305@gmail.com>
2023-09-19 22:56:27 +0900

    Finished Section9

--- AppBuild/6-네비게이션바와탭바/N&TFirstCode/N&TFirstCode/LoginViewController.swift
+++ AppBuild/6-네비게이션바와탭바/N&TFirstCode/N&TFirstCode/LoginViewController.swift
@@ -45,12 +45,12 @@ class LoginViewController: UIViewController {
     @objc func loginButtonTapped() {
         
         // 전화면의 isLoggedIn속성에 접근하기 ⭐️
-//        if let presentingVC = presentingViewController { // 옵셔널 바인딩
-//            let tabBarCon = presentingVC as! UITabBarController   // 탭바에 접근하기
-//            let nav = tabBarCon.viewControllers?[0] as! UINavigationController  // 네비게이션바에 접근하기
-//            let firstVC = nav.viewControllers[0] as! FirstViewController  // FirstVC에 접근하기
-//            firstVC.isLoggedIn.toggle()  // 로그인 되었다고 상태 변화시키기 (실제 앱에서 이렇게 구현할 일은 없음)
-//        }
+        if let presentingVC = presentingViewController { // 옵셔널 바인딩
+            let tabBarCon = presentingVC as! UITabBarController   // 탭바에 접근하기
+            let nav = tabBarCon.viewControllers?[0] as! UINavigationController  // 네비게이션바에 접근하기
+            let firstVC = nav.viewControllers[0] as! FirstViewController  // FirstVC에 접근하기
+            firstVC.isLoggedIn.toggle()  // 로그인 되었다고 상태 변화시키기 (실제 앱에서 이렇게 구현할 일은 없음)
+        }
         
         dismiss(animated: true, completion: nil)
     }
