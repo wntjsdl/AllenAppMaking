7dc71063f8710a4f843fe600f023105e44411686
Sunny <wntjs0305@gmail.com>
2023-09-21 22:50:27 +0900

    Custom Delegate Pattern 3/9

---
+++ AppBuild/8-커스텀델리게이트패턴/MemberList_start/MemberList_start/Views/MyTableViewCell.swift
@@ -0,0 +1,105 @@
+//
+//  MyTableViewCell.swift
+//  MemberList_start
+//
+//  Created by JuSun Kang on 2023/09/21.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    // MARK: - UI구현
+    let mainImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let memberNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let addressLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 10)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let stackView: UIStackView = {
+        let sv = UIStackView()
+        sv.axis = .vertical
+        sv.distribution = .fill
+        sv.alignment = .fill
+        sv.spacing = 5
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        return sv
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+        setupStackView()
+    }
+    
+    func setupStackView() {
+        self.addSubview(mainImageView)
+        
+        self.addSubview(stackView)
+        
+        stackView.addArrangedSubview(memberNameLabel)
+        stackView.addArrangedSubview(addressLabel)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+  
+    // 코드를 사용했기 때문에 필요 없음
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+    
+    override func updateConstraints() {
+        setConstraints()
+        super.updateConstraints()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.mainImageView.clipsToBounds = true
+        self.mainImageView.layer.cornerRadius = self.mainImageView.frame.width / 2
+    }
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            mainImageView.heightAnchor.constraint(equalToConstant: 40),
+            mainImageView.widthAnchor.constraint(equalToConstant: 40),
+            mainImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            mainImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            memberNameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: mainImageView.trailingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: self.mainImageView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: self.mainImageView.bottomAnchor)
+        ])
+    }
+
+}
