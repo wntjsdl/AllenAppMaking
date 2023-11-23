60f57688c3abec5641e788956dbafdcf9651910b
Sunny <wntjs0305@gmail.com>
2023-11-23 23:48:38 +0900

    Practice-master Chapter6.6.6 Add DepartmentList Func ~1064p

--- Swift-Practice-master/Chapter06/Chapter06-HR/Chapter06-HR/DepartmentListVC.swift
+++ Swift-Practice-master/Chapter06/Chapter06-HR/Chapter06-HR/DepartmentListVC.swift
@@ -8,22 +8,48 @@
 import UIKit
 
 class DepartmentListVC: UITableViewController {
+    
+    var departList: [(departCd: Int, departTitle: String, departAddr: String)]!
+    let departDAO = DepartmentDAO()
+    
+//    typealias Foo = (name: String, age: Int)
+//    let s: Foo = (name: "홍길동", age: 25)
+//    print("\(s.name)은 \(s.age)세입니다")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.departList = self.departDAO.find()
+        self.initUI()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let dummyView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        self.tableView.tableFooterView = dummyView
+    }
+    
+    func initUI() {
+        // 1. 내비게이션 타이틀용 레이블 속성 설정
+        let navTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 60))
+        navTitle.numberOfLines = 2
+        navTitle.textAlignment = .center
+        navTitle.font = UIFont.systemFont(ofSize: 14)
+        navTitle.text = "부서 목록 \n" + " 총 \(self.departList.count) 개"
+        
+        // 2. 내비게이션 바 UI 설정
+        self.navigationItem.titleView = navTitle
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        
+        // 3. 셀을 스와이프 했을 때 편집 모드가 되도록 설정
+        self.tableView.allowsFocusDuringEditing = true
+    }
+    
+    @objc func editing(_ sender: Any) {
+        self.tableView.setEditing(true, animated: true)
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return self.departList.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -31,16 +57,45 @@ class DepartmentListVC: UITableViewController {
         return 0
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let rowData = self.departList[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DEPART_CELL")
+        
+        cell?.textLabel?.text = rowData.departTitle
+        cell?.textLabel?.font = UIFont.systemFont(ofSize: 14)
+        
+        cell?.detailTextLabel?.text = rowData.departAddr
+        cell?.detailTextLabel?.font = UIFont.systemFont(ofSize: 12)
 
-        // Configure the cell...
-
-        return cell
+        return cell!
     }
-    */
 
+    @IBAction func add(_ sender: Any) {
+        let alert = UIAlertController(title: "신규 부서 등록", message: "신규 부서를 등록해 주세요", preferredStyle: .alert)
+        
+        alert.addTextField { tf in tf.placeholder = "부서명" }
+        alert.addTextField { tf in tf.placeholder = "주소" }
+        
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel))
+        alert.addAction(UIAlertAction(title: "확인", style: .default) { _ in
+            let title = alert.textFields?[0].text
+            let addr = alert.textFields?[1].text
+            
+            if self.departDAO.create(title: title!, addr: addr!) {
+                self.departList = self.departDAO.find()
+                self.tableView.reloadData()
+                
+                let navTitle = self.navigationItem.titleView as! UILabel
+                navTitle.text = "부서 목록 \n" + " 총 \(self.departList.count) 개"
+            }
+        })
+        self.present(alert, animated: false)
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return UITableViewCell.EditingStyle.delete
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -49,17 +104,15 @@ class DepartmentListVC: UITableViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
+        let departCd = self.departList[indexPath.row].departCd
+        
+        if departDAO.remove(departCd: departCd) {
+            self.departList.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
     /*
     // Override to support rearranging the table view.
