4bb409ce9ce96b071bc2cda3fbe11fdae55e9267
Sunny <wntjs0305@gmail.com>
2024-01-09 23:28:55 +0900

    MVVM Conversion Challenge2

---
+++ AppBuild/MVVM/활용-8.커스텀델리게이트(테이블뷰)/MemberList-MVC (with NoView) Challenge3/MemberList/ViewModels/MemberListViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  MemberListViewModel.swift
+//  MemberList
+//
+//  Created by JuSun Kang on 1/9/24.
+//
+
+import UIKit
+
+class MemberListViewModel {
+    
+    let dataManager: MemberListType
+    
+    let title: String
+    
+    init(dataManager: MemberListType, title: String) {
+        self.dataManager = dataManager
+        self.title = title
+    }
+    
+    // MARK: - Output
+    
+    
+    
+    // MARK: - Input
+    
+    
+    
+    // MARK: - Logic
+    
+}
---
+++ AppBuild/MVVM/활용-8.커스텀델리게이트(테이블뷰)/MemberList-MVC (with NoView) Challenge3/MemberList/ViewModels/MemberViewModel.swift
@@ -0,0 +1,98 @@
+//
+//  MemberViewModel.swift
+//  MemberList
+//
+//  Created by JuSun Kang on 1/9/24.
+//
+
+import UIKit
+
+class MemberViewModel {
+    
+    let dataManager: MemberListType
+    
+    private var member: Member?
+    private let index: Int?
+    
+    init(dataManager: MemberListType, member: Member? = nil, index: Int? = nil) {
+        self.dataManager = dataManager
+        self.member = member
+        self.index = index
+    }
+    
+    // MARK: - Output
+    
+    var memberImage: UIImage? {
+        member?.memberImage
+    }
+    
+    var memberIdString: String? {
+        String(member?.memberId ?? Member.memberNumbers)
+    }
+    
+    var nameString: String? {
+        member?.name
+    }
+    
+    var phoneString: String? {
+        member?.phone
+    }
+    
+    var addressString: String? {
+        member?.address
+    }
+    
+    var ageString: String? {
+        member != nil ? String(member!.age!) : ""
+    }
+    
+    var buttonTitle: String {
+        member != nil ? "UPDATE" : "SAVE"
+    }
+    
+    
+    
+    
+    // MARK: - Input
+    
+    func handleButtonTapped(image: UIImage?, name: String?, age: String?, phone: String, address: String) {
+        if let member = member {
+            updateMember(image: image, name: name, age: age, phone: phone, address: address)
+        } else {
+            saveNewMember(image: image, name: name, age: age, phone: phone, address: address)
+        }
+    }
+    
+    
+    
+    // MARK: - Logic
+    
+    func updateMember(image: UIImage?, name: String?, age: String?, phone: String, address: String) {
+        
+        guard let member = self.member, let index = self.index else { return }
+        
+        let ageInt = Int(age ?? "") ?? 0
+        
+        self.member = Member(exitingMember: member, image: image, name: name, age: ageInt, phone: phone, address: address)
+        
+        dataManager.updateMemberInfo(index: index, with: member)
+    }
+    
+    func saveNewMember(image: UIImage?, name: String?, age: String?, phone: String, address: String) {
+        
+        let ageInt = Int(age ?? "") ?? 0
+        
+        let member = Member(image: image, name: name, age: ageInt, phone: phone, address: address)
+        
+        dataManager.makeNewMember(member)
+    }
+    
+    func backToBeforeVC(fromCurrentVC: UIViewController, animated: Bool) {
+        let currentVC = fromCurrentVC
+        currentVC.navigationController?.popViewController(animated: animated)
+    }
+    
+    
+    
+    
+}
